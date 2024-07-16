b5f0abdc3fad17dfd0b893634a4448f4cd6ec7ce
Fiedler12 <o.lyngholm@gmail.com>
2024-07-16 16:02:51 +0200

    Initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,93 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useState } from "react"
+
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false); 
+    const [failed, setFailed] = useState(false);
+    const router = useRouter();
+
+    const submit = async () => {
+        setLoading(true);
+        await setTimeout(() => {console.log("Login started")}, 1000);
+        router.push("/home");        
+    }
+    return(
+        <div className="flex min-h-full flex-1 flex-col px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-100 w-auto"
+            src="/lad.png"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-black dark:text-white">
+            Velkommen lads
+          </h2>
+        </div>
+        {!loading ?
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action={submit}>
+            {failed && 
+            <div>
+              <label className="block text-sm font-medium leading-6 text-red-500">
+                Wrong username or password
+              </label>
+            </div>
+            }
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-black dark:text-white">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  onChange={e => setUsername(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-black dark:text-white">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  onChange={e => setPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-primary-shade-75 border-gray-200 dark:bg-gray-900 dark:border-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-grey-600"
+                >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+        : 
+        <div>
+            <h1>Loading</h1>
+
+        </div> 
+        }
+        </div>
+    )
+}
